b12faf7fd21fb11fe5adf98530e2088c8a2cfd18
Msugh623 <iternenge469@gmail.com>
2023-11-24 13:55:27 +0100

    color palate ready with color picker

---
+++ palate.js
@@ -0,0 +1,46 @@
+const container = document.getElementById('scratchcontainer');
+const pixels = document.getElementsByClassName('pixel');
+const hidden = document.getElementById('hidden');
+const colorPicker = document.getElementById('colorPicker')
+const clear = document.getElementById('clear')
+
+const lsp = JSON.parse(localStorage.getItem('pixels'));
+const pixel = "<div class='pixel'></div>";
+const pixelNum = 3000;
+
+let currentColor = '#232323';
+
+const doTheNeedful = () => {
+    for (let i = 0; i < pixelNum; i++) {
+        container.innerHTML = ` ${container.innerHTML} ${pixel}`;
+    }
+}
+const fromLst = () => container.innerHTML = lsp?.data.map(() => pixel);
+
+lsp ? fromLst() : doTheNeedful();
+
+for (let i = 0; i < pixels.length; i++) {
+    pixels[i].addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = currentColor;
+    })
+}
+
+colorPicker.onchange = (e) => {
+    const { value } = e.target;
+    currentColor = value;
+}
+
+clear.onclick = () => {
+    fromLst();
+    for (let i = 0; i < pixels.length; i++) {
+        pixels[i].addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = currentColor;
+        })
+    }
+}
+
+console.log(lsp, currentColor);
+hidden.style.display = 'block';
+colorPicker.value = currentColor;
+lsp.data.length
+    && localStorage.setItem('pixels', JSON.stringify({ data: [...pixels] }));
