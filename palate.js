bb015cc52ed32f5b7964ad6357e2eb1f7d9db020
Msugh623 <126330083+Msugh623@users.noreply.github.com>
2024-05-01 12:00:27 +0100

    first commit

--- palate.js
+++ palate.js
@@ -1,10 +1,10 @@
-const container = document.getElementById('scratchcontainer');
+const container = document.getElementById('whiteBoard');
 const pixels = document.getElementsByClassName('pixel');
 const hidden = document.getElementById('hidden');
 const colorPicker = document.getElementById('colorPicker')
 const clear = document.getElementById('clear')
+const nav = document.getElementById('nav')
 
-const lsp = JSON.parse(localStorage.getItem('pixels'));
 const pixel = "<div class='pixel'></div>";
 const pixelNum = 3000;
 
@@ -14,15 +14,17 @@ const doTheNeedful = () => {
     for (let i = 0; i < pixelNum; i++) {
         container.innerHTML = ` ${container.innerHTML} ${pixel}`;
     }
+    lsp = { data: [...pixels] }
 }
+
 const fromLst = () => container.innerHTML = lsp?.data.map(() => pixel);
 
 lsp ? fromLst() : doTheNeedful();
 
 for (let i = 0; i < pixels.length; i++) {
-    pixels[i].addEventListener('mouseover', (e) => {
+    pixels[i].onmouseenter=(e) => {
         e.target.style.backgroundColor = currentColor;
-    })
+    }
 }
 
 colorPicker.onchange = (e) => {
@@ -33,13 +35,25 @@ colorPicker.onchange = (e) => {
 clear.onclick = () => {
     fromLst();
     for (let i = 0; i < pixels.length; i++) {
-        pixels[i].addEventListener('mouseover', (e) => {
+        pixels[i].onmouseenter=(e) => {
             e.target.style.backgroundColor = currentColor;
-        })
+        }
     }
 }
 
+
 hidden.style.display = 'block';
 colorPicker.value = currentColor;
-lsp.data.length
+lsp?.data?.length
     && localStorage.setItem('pixels', JSON.stringify({ data: [...pixels] }));
+
+save.onclick = () => {
+    print()
+}
+
+onbeforeprint = () => {
+    nav.classList.add('d-none')
+}
+onafterprint = () => {
+    nav.classList.remove('d-none')
+}
